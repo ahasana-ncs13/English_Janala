67cc6f9f79710f27ab1bf403630afc67ca3e03f3
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-09-06 03:28:48 +0600

    added lessons , details ,modal

--- script.js
+++ script.js
@@ -5,15 +5,109 @@ const APIlesson=()=>{
 }
 APIlesson()
 
+let wordDetail= (id)=>{
+    let url= `https://openapi.programming-hero.com/api/word/${id}`
+    console.log(url);
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayWordDetail(data.data));
+}
+
+let displayWordDetail=(details)=>{
+    let detailsContainer= document.getElementById("details-container");
+    detailsContainer.innerHTML=`
+       <div class="details-card p-6">
+            <h2 class="font-semibold text-4xl">${details.word} (<i class="fa-solid fa-microphone-lines"></i>:<span class="hind-siliguri-regular"> ${details.pronunciation} </span>)</h2>
+            <div class="meaning mt-8">
+            <p class="font-semibold text-2xl">Meaning</p>
+            <p class="hind-siliguri-regular font-medium text-2xl mt-2">${details.meaning}</p>
+            </div>
+            <div class="example my-8">
+            <p class="font-semibold text-2xl">Example</p>
+            <p class="text-2xl mt-2">${details.sentence}</p>
+            </div>
+            <p class="hind-siliguri-regular font-medium text-2xl mb-3">সমার্থক শব্দ গুলো</p>
+            <div class="text-2xl flex justify-start gap-5 mb-6">
+                <p class="bg-[#EDF7FF] rounded-2xl p-4">${details.synonyms[0]}</p>
+                <p class="bg-[#EDF7FF] rounded-2xl p-4">${details.synonyms[1]}</</p>
+                <p class="bg-[#EDF7FF] rounded-2xl p-4">${details.synonyms[2]}</</p>
+            </div>
+            <button class="btn btn-primary">Complete Learning</button>
+        </div>
+    `
+    document.getElementById("my_modal_5").showModal()
+   
+}
+
+let lessonWord = (id)=>{
+    let url=`https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let lessonBtn =document.getElementById(`lesson-btn-${id}`);
+        lessonRemove();
+        lessonBtn.classList.add("active");
+        console.log(lessonBtn);
+        lessoncard(data.data);
+        
+    } );
+}
+let lessonRemove = ()=>{
+    let removeBtn= document.querySelectorAll(".lesson-btn");
+    removeBtn.forEach(rem => {
+        rem.classList.remove("active");
+    });
+}
+
+let lessoncard = (words)=>{
+    let LessonCardContainer = document.getElementById("Lessoncard-container")
+    LessonCardContainer.innerHTML=" ";
+    if(words.length == 0){
+        LessonCardContainer.innerHTML=`
+        <div class="selectLesson-container text-center col-span-full py-10">
+               <div class="flex justify-center mb-5">
+               <img src="./assets/alert-error.png" alt="" >
+                </div>
+        
+                <p class="text-[#79716B] hind-siliguri-regular text-sm mb-4">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                
+                <h2 class="font-medium text-3xl text-[#292524] hind-siliguri-regular">নেক্সট Lesson এ যান</h2>
+              </div>
+        `
+        return;
+    }
+
+    words.forEach(word=> {
+        let cardDiv= document.createElement("div");
+        cardDiv.innerHTML=`
+        <div class=" bg-white rounded-xl text-center p-10">
+            <h2 class="font-bold text-3xl">${word.word? word.word:"word is not found"}</h2>
+            <p class="text-xl font-medium my-6">Meaning /Pronounciation</p>
+            <p class="hind-siliguri-regular font-semibold text-3xl text-[#18181B]">${word.meaning?word.meaning:"meaning is not found"} / ${word.pronunciation?word.pronunciation:"pronunciation is not found"}</p>
+            <div class="icon flex justify-between mt-5">
+
+                   <button onclick="wordDetail(${word.id})" class="btn bg-[#1A91FF]/10">
+                    <i class="fa-solid fa-circle-info"></i>
+                     </button>
+
+                    <button class="btn bg-[#1A91FF]/10">
+                    <i class="fa-solid fa-volume-low"></i>
+                     </button>
+            </div>
+          </div>
+        `
+        LessonCardContainer.append(cardDiv);
+    });
+}
+
 const displayLesson=(lessons)=>{
-    console.log(lessons.data)
       let lessonContainer = document.getElementById("learn-container");
-      lessonContainer.innerHTML=" "
+      lessonContainer.innerHTML=" ";
     for (const lesson of lessons.data) {
         
           let lessonCard = document.createElement("div");
           lessonCard.innerHTML=`
-          <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no} </button> 
+          <button id="lesson-btn-${lesson.level_no}" onclick="lessonWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no} </button> 
          `
         lessonContainer.append(lessonCard);
     }
